72f652b2af73eeda44f6a83abe6ef29489a87a2f
lukeyangskywalker <lukeyangskywalker@outlook.com>
2014-04-15 19:19:13 -0400

    setting up LibGDX

---
+++ FlappyMario-desktop/src/com/skywalker/FlappyMario/Main.java
@@ -0,0 +1,15 @@
+package com.skywalker.FlappyMario;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "FlappyMario";
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new FlappyMarioGame(), cfg);
+	}
+}
